61a28d6a61e8213cc3b0a24275a27bf9c048c512
Mike McTighe <m.mctighe10@gmail.com>
2019-09-16 17:36:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,18 +6,5 @@ def name = (dog_name)
   def name
     @this_dogs_name
   end
-  def 
+  def
 end
-
-class Dog
-  def name=(dog_name)
-      @this_dogs_name = dog_name
-    end
-
-    def name
-      @this_dogs_name
-    end
-end
-lassie = Dog.new
-lassie.name = "Lassie"
-puts lassie.name
